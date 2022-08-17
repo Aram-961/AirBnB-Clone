a5e9b33717534ba9c37348da29273c3f9c59b5ba
aramistamboulian961 <aramistamboulian961@gmail.com>
2022-08-17 23:27:23 +0300

    finishing responsive

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import dataUser from './UserData'
+import React from "react";
+import dataUser from "./UserData";
 
 const Card = () => {
     return (
-        <div className='cards'>
-            {
-                dataUser.map((item, index) => (
-                    <div class="card--details" key={index}>
+        <div className="cards">
+            {dataUser.map((item) => {
+                let badgeChecker;
+                if (item.openSpots === 0) {
+                    badgeChecker = "SOLD OUT";
+                } else if (item.location === "Online") {
+                    badgeChecker = "ONLINE";
+                }
+                return (
+                    <div className="card--details" key={item.id}>
+                        {badgeChecker && <div className="card--badge">{badgeChecker}</div>}
                         <img src={item.coverImg} className="card--img" alt="" />
-                        <div className='card-stats'>
-                            <h1 className='rating'>{item.stats.rating}</h1>
-                            <h2 class="review-count">({item.stats.reviewCount}) .online</h2>
+                        <div className="card-stats">
+                        <img src='/star.png' className="star--rating" />
+                            <h1 className="rating">{item.stats.rating}</h1>
+                            <span className="review-count">({item.stats.reviewCount})</span>
+                            <span className="location--card">{item.location}</span>
                         </div>
                         <p className="title">{item.title}</p>
                         <h2 className="price-details">From $ {item.price} / person</h2>
                     </div>
-                ))
-            }
+                );
+            })}
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
